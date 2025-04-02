b9691487976aef89095ed300f762fb841e5acb10
Cakey25 <cakey25yt@gmail.com>
2025-04-02 09:51:57 +0100

    improving player drag

--- player.py
+++ player.py
@@ -37,15 +37,18 @@ class Player:
 
     def update(self):
         
+        # Moving forwards and backwards
         if self.app.keys[pg.K_w] and not self.app.keys[pg.K_s]:
             self.rigid.apply_force_local(
                 force=pg.Vector2(20, 0),
                 relative_pos=pg.Vector2(0 , 0))
+
         if self.app.keys[pg.K_s] and not self.app.keys[pg.K_w]:
             self.rigid.apply_force_local(
                 force=pg.Vector2(-20, 0),
                 relative_pos=pg.Vector2(0, 0))
-        
+
+        # Rotating clockwise and anticlockwise 
         if self.app.keys[pg.K_d] and not self.app.keys[pg.K_a]:
             self.rigid.apply_force_local(
                 force=pg.Vector2(0, 10),
@@ -53,6 +56,7 @@ class Player:
             self.rigid.apply_force_local(
                 force=pg.Vector2(0, -10),
                 relative_pos=pg.Vector2(1, 0))
+
         if self.app.keys[pg.K_a] and not self.app.keys[pg.K_d]:
             self.rigid.apply_force_local(
                 force=pg.Vector2(0, -10),
@@ -61,42 +65,30 @@ class Player:
                 force=pg.Vector2(0, 10),
                 relative_pos=pg.Vector2(1, 0))
 
-        # Drag
-        if self.rigid.vel.length() != 0:
-            proportional_drag = -self.rigid.vel.normalize() * self.rigid.vel.dot(self.rigid.vel) * 1
-        else:
-            proportional_drag = pg.Vector2(0, 0)
-
-        self.rigid.apply_force_external(proportional_drag, pg.Vector2(0, 0))
-
-        if self.rigid.vel.length() != 0:
-            linear_drag = -self.rigid.vel.normalize() * 0.2 # Drag coefficient
-        else:
+        # Linear drag
+        if self.rigid.vel.length_squared() != 0:
             linear_drag = pg.Vector2(0, 0)
+            velocity_normalized = self.rigid.vel.normalize()
+            # Calculate the linear drag
+            linear_drag += 1 * -velocity_normalized * self.rigid.vel.dot(self.rigid.vel)
+            linear_drag += 0 * -velocity_normalized * self.rigid.vel.length()
+            linear_drag += 0.2 * -velocity_normalized
+            # Apply drag force
+            self.rigid.apply_force_external(linear_drag, pg.Vector2(0, 0))
 
-        self.rigid.apply_force_external(linear_drag, pg.Vector2(0, 0))
-
-        # Angular drag
-        relative_point = pg.Vector2(1, 0)
+        # Angular drag 
         if self.rigid.angular_vel != 0:
-            proportional_angular_drag = -0.5 * self.rigid.angular_vel ** 3 / abs(self.rigid.angular_vel)
-        else:
-            proportional_angular_drag = 0
-        rotational_force = pg.Vector2(0, 1) * proportional_angular_drag
-
-        self.rigid.apply_force_local(rotational_force, relative_point)
-        self.rigid.apply_force_local(rotational_force * -1, relative_point * -1)
-
-        if self.rigid.angular_vel != 0:
-            linear_angular_vel = -0.2 * self.rigid.angular_vel / abs(self.rigid.angular_vel)
-        else:
-            linear_angular_vel = 0
-        rotational_force = pg.Vector2(0, 1) * linear_angular_vel
-
-        self.rigid.apply_force_local(rotational_force, relative_point)
-        self.rigid.apply_force_local(rotational_force * -1, relative_point * -1)
-
-        # This is bad code
+            relative_point = pg.Vector2(-1, 0)
+            drag_direction = self.rigid.angular_vel / abs(self.rigid.angular_vel)
+            angular_drag = 0
+            # Calculate the angular drag
+            angular_drag += 0.5 * -drag_direction * self.rigid.angular_vel ** 2
+            angular_drag += 0 * -drag_direction * self.rigid.angular_vel
+            angular_drag += 0.2 * -drag_direction
+            # Apply drag force
+            rotational_force = pg.Vector2(0, angular_drag)
+            self.rigid.apply_force_local(rotational_force, -relative_point)
+            self.rigid.apply_force_local(-rotational_force, relative_point)
         
         self.rigid.update()
 
